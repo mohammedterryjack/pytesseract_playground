73e75fb5e1af3834609d4475bb2724b31d78cc70
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2023-06-13 13:55:49 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,69 @@
 from pytesseract import get_tesseract_version
-from streamlit import file_uploader, image, set_page_config, tabs
+from streamlit import file_uploader, image, set_page_config, sidebar, slider, tabs
 
-from utils import binarise, display_text, grayscale, open_image
+from utils import (
+    binarise,
+    display_text,
+    draw_bounding_boxes,
+    grayscale,
+    invert,
+    open_image,
+)
 
 set_page_config(
     page_title=f"Tesseract {get_tesseract_version()}",
     page_icon="📝",
-    initial_sidebar_state="expanded",
 )
 image_display_size = 300
 uploaded_file = file_uploader("Choose an image")
+distance = sidebar.slider("Min Distance from Field", 0, 100, 80)
+show_boxes = sidebar.checkbox("Show Fields")
 original_tab, grayscale_tab, binary_tab = tabs(["Original", "Grayscale", "Binary"])
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     image_data = open_image(image_bytes=bytes_data)
     gray_data = grayscale(image_data=image_data)
     binary_data = binarise(image_data=gray_data)
+    binary_data_inverted = invert(binary_data)
     with original_tab:
-        image(bytes_data, width=image_display_size)
-        display_text(image_data=image_data, name="original")
+        bytes_data_bboxes, coordinates_fields, coordinates_boxes = draw_bounding_boxes(
+            image=image_data, binarised_image=binary_data
+        )
+        image(
+            bytes_data_bboxes if show_boxes else bytes_data,
+            width=image_display_size,
+            channels="BGR" if show_boxes else "RGB",
+        )
+        display_text(
+            image_data=image_data,
+            name="original",
+            coordinates=coordinates_fields + coordinates_boxes,
+            min_field_distance=distance,
+        )
     with grayscale_tab:
-        image(gray_data, width=image_display_size)
-        display_text(image_data=gray_data, name="grayscale")
+        gray_data_bboxes, coordinates_fields, coordinates_boxes = draw_bounding_boxes(
+            image=gray_data, binarised_image=binary_data
+        )
+        image(gray_data_bboxes if show_boxes else gray_data, width=image_display_size)
+        display_text(
+            image_data=gray_data,
+            name="grayscale",
+            coordinates=coordinates_fields + coordinates_boxes,
+            min_field_distance=distance,
+        )
     with binary_tab:
-        image(binary_data, width=image_display_size)
-        display_text(image_data=binary_data, name="binary")
+        (
+            binary_data_inverted_bboxes,
+            coordinates_fields,
+            coordinates_boxes,
+        ) = draw_bounding_boxes(image=binary_data_inverted, binarised_image=binary_data)
+        image(
+            binary_data_inverted_bboxes if show_boxes else binary_data_inverted,
+            width=image_display_size,
+        )
+        display_text(
+            image_data=binary_data,
+            name="binary",
+            coordinates=coordinates_fields + coordinates_boxes,
+            min_field_distance=distance,
+        )
--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
 from io import BytesIO
-from typing import Dict, Generator, List
+from typing import Dict, Generator, List, Tuple
 
 from cv2 import (
+    CHAIN_APPROX_SIMPLE,
     COLOR_BGR2GRAY,
+    MORPH_DILATE,
+    MORPH_RECT,
+    RETR_TREE,
     THRESH_BINARY_INV,
     THRESH_OTSU,
     bitwise_not,
+    boundingRect,
     cvtColor,
+    findContours,
+    getStructuringElement,
+    morphologyEx,
+    rectangle,
     threshold,
 )
 from numpy import array, ndarray
@@ -15,13 +24,23 @@ from pytesseract import image_to_data
 from streamlit import cache_data, form, form_submit_button, slider, write
 
 
-def display_text(image_data: ndarray, name: str) -> None:
+def display_text(
+    image_data: ndarray,
+    name: str,
+    coordinates: List[Tuple[int, int]],
+    min_field_distance: int,
+) -> None:
     with form(key=name):
         confidence = slider("Minimum Confidence", 0, 100)
         if form_submit_button("Extract Text"):
             results = extract_text_from_image(image_data=image_data)
-            confident_results = filter_results_below_confidence(
-                results=results, confidence=confidence
+            confident_results = list(
+                filter_results_below_confidence(results=results, confidence=confidence)
+            )
+            confident_results = annotate_field_text(
+                results=confident_results,
+                coordinates_of_fields=coordinates,
+                min_distance_threshold=min_field_distance,
             )
             grouped_text = group_text_by_block_number(results=confident_results)
             for text in grouped_text:
@@ -30,7 +49,11 @@ def display_text(image_data: ndarray, name: str) -> None:
 
 def binarise(image_data: ndarray) -> ndarray:
     _, image_threshold = threshold(image_data, 0, 255, THRESH_BINARY_INV | THRESH_OTSU)
-    return bitwise_not(image_threshold)
+    return image_threshold
+
+
+def invert(image_data: ndarray) -> ndarray:
+    return bitwise_not(image_data)
 
 
 def grayscale(image_data: ndarray) -> ndarray:
@@ -69,6 +92,8 @@ def group_text_by_block_number(
         text = result["text"]
         if not text:
             continue
+        if result["field"]:
+            text = f":red[{text}]"
         n = result["block_num"]
         if n in sections:
             sections[n] += delimiter + text
@@ -87,3 +112,69 @@ def format_ocr_result_as_dictionary(
         values = line.split(key_delimiter)
         key_value_pairs = zip(keys, values)
         yield dict(key_value_pairs)
+
+
+def blur_image(image_data: ndarray) -> ndarray:
+    return morphologyEx(
+        image_data, MORPH_DILATE, getStructuringElement(MORPH_RECT, (3, 3))
+    )
+
+
+def annotate_field_text(
+    results: List[Dict[str, str]],
+    coordinates_of_fields: List[Tuple[int, int]],
+    min_distance_threshold: int,
+) -> List[Dict[str, str]]:
+    for result in results:
+        if (
+            "left" not in result
+            or "top" not in result
+            or not any(coordinates_of_fields)
+        ):
+            result["field"] = False
+            continue
+        x = int(result["left"])
+        y = int(result["top"])
+        nearest_coordinates = min(
+            coordinates_of_fields,
+            key=lambda coordinate: l2_distance(
+                coordinates1=coordinate, coordinates2=(x, y)
+            ),
+        )
+        nearest_distance = l2_distance(
+            coordinates1=nearest_coordinates, coordinates2=(x, y)
+        )
+        result["field"] = nearest_distance < min_distance_threshold
+    return results
+
+
+def l2_distance(coordinates1: Tuple[int, int], coordinates2: Tuple[int, int]) -> float:
+    x1, y1 = coordinates1
+    x2, y2 = coordinates2
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
+@cache_data
+def draw_bounding_boxes(
+    image: ndarray,
+    binarised_image: ndarray,
+    noise_threshold: int = 1000,
+    checkbox_threshold: int = 50,
+) -> Tuple[ndarray, List[Tuple[int, int]], List[Tuple[int, int]]]:
+    bbox_image = image.copy()
+    image_blurred = blur_image(image_data=binarised_image)
+    contours, _ = findContours(image_blurred, RETR_TREE, CHAIN_APPROX_SIMPLE)
+
+    field_coordinates, checkbox_coordinates = [], []
+
+    for contour in contours:
+        x, y, w, h = boundingRect(contour)
+        if w * h < noise_threshold:
+            continue
+        if abs(w - h) <= checkbox_threshold:
+            rectangle(bbox_image, (x, y), (x + w, y + h), (255, 0, 0), 3)
+            checkbox_coordinates.append((x, y))
+        if w > h:
+            rectangle(bbox_image, (x, y), (x + w, y + h), (255, 0, 255), 3)
+            field_coordinates.append((x, y))
+    return bbox_image, field_coordinates, checkbox_coordinates
